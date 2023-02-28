31e933d762cd238b41187afb37240eb9a1f10938
mipiticuenta <mi.piticuenta@gmail.com>
2023-02-28 20:26:28 +0100

    update

--- github_commit.sh
+++ github_commit.sh
@@ -3,7 +3,7 @@
 # <commit operations for all local changes>
 
 git fetch
-git log --all --oneline --graph --decorate
+# git log --all --oneline --graph --decorate
 git add --all
 git status
 git commit -am "update"
