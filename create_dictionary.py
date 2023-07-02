1dd9241b35c651aa0103b300d7b5abb325466274
mipiticuenta <mi.piticuenta@gmail.com>
2023-07-03 01:22:22 +0200

    update

--- create_dictionary.py
+++ create_dictionary.py
@@ -1,13 +1,15 @@
+""" Create a dictionary from textfile """
+
+# <settings>
+
 import re                   # Regular expression operations
 import pandas as pd
 import numpy as np
 import progressbar as pb    # Progress bar
 
-# <settings>
-
 file3_out_name = "dictionary"
 
-# <settings>
+# </settings>
 
 print('###############################################################################')
 print('#')
@@ -26,7 +28,7 @@ list1_in = set(line.strip() for line in open(file1_in, encoding='Latin1'))
 list1_in = sorted(list1_in)
 print(len(list1_in), 'lines read')
 
-# <get filename containing text lists, convert into list, sort and dedup>
+# </get filename containing text lists, convert into list, sort and dedup>
 
 # <open file3_out file and write header>
 
@@ -34,25 +36,30 @@ file3_out = open(file3_out_name, 'w', encoding='Latin1')
     # file3_out.write('! Clean block_list')
     # file3_out.write('\n')
 
-# <open file3_out file and write header>
+# </open file3_out file and write header>
 
 list3_out = []
 
 # <create dictionary>
 
 print('creating dictionary')
-progress = pb.ProgressBar(maxval = len(list1_in)).start()
-progvar = 0
+pbar = pb.ProgressBar(maxval = len(list1_in)).start()
+progress = 0
+
 
 for line in list1_in:
     line = re.split(r"[^a-z,^A-Z]+", line)
     for word in line: list3_out.append(word)
-    progress.update(progvar + 1)
-    progvar += 1
-print('')
+    progress += 1
+    pbar.update(progress)
+print('\n')
 
-print('done!')
-print('')
+pbar.finish()
+print(
+    '\n'
+    + 'done!'
+    + '\n'
+)
 
 list3_out_pd = pd.DataFrame(list3_out)
 list3_out_pd.columns = ['word']
@@ -65,4 +72,4 @@ print(len(list3_out_pd), 'lines written')
 print('Find results in textfile <' + file3_out_name + '>')
 print('\n')
 
-# <create dictionary>
+# </create dictionary>
