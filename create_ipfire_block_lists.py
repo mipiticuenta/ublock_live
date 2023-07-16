9f554b98887ef732e31d18d4b11effa5b77662fd
mipiticuenta <mi.piticuenta@gmail.com>
2023-07-16 19:49:18 +0200

    update

--- create_ipfire_block_lists.py
+++ create_ipfire_block_lists.py
@@ -83,7 +83,7 @@ for line in list1_in:
     if len(line) > 0:                      # <remove empty lines/>
         string_r = ''
         if string_r := re.search(r'#', line):
-            list1_out.add(line)            # <cosmetics filters are case sensitive/>
+            list1_out.add(line)            # <cosmetics filters are case-sensitive/>
         else :
             list1_out.add(line.lower())    # <transform to lower case/>
 
