564057200ee40f5a5a74075263f256b088100788
mipiticuenta <mi.piticuenta@gmail.com>
2023-08-11 11:41:40 +0200

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -2,7 +2,6 @@
 
 import os                   # Miscellaneous operating system interfaces
 import re                   # Regular expression operations
-import progressbar as pb    # Progress bar
 import requests             # Get files using url
 import time                 # Time functions
 import tqdm                 # Progress bar
@@ -46,11 +45,11 @@ i        = 1    # <counter for uncommented sources/>
 
 for line in list1 :
     print(
-        'reading',
+        'reading source',
         '{:3.0f}'.format(i),
         '/',
         len(list1),
-        'sources listed', ':',
+        ':',
         line
         )
     i += 1
@@ -74,54 +73,54 @@ del(list1)    # <clean up; make sure list1_in is not used anymore hereafter/>
 print('Removing unnecessary spaces, lines, comments, etc; applying lower case except for case-sensitive filters')
 print('--------------------------------------------------------------------------------------------------------')
 
-print('1.1 : dedup spaces and remove leading/trailing spaces')
+print(' 1/16 : dedup spaces and remove leading/trailing spaces')
 list2 = [re.sub(r' +', ' ', line).strip()                      for line in list2]    # <dedup spaces and remove leading/trailing spaces/>
 
-print('1.2 : remove uBO style trailing comments')
+print(' 2/16 : remove uBO style trailing comments')
 list2 = [re.sub(r'!.*', '', line).strip()                      for line in list2]    # <remove uBO style comments'/>
 
-print('1.3 : remove not uBO style trailing comments')
+print(' 3/16 : remove not uBO style trailing comments')
 list2 = [re.sub(r'#.*', '', line).strip()                      for line in list2]    # <remove not uBO style trailing comments'/>
 list2 = [re.sub(r'^#(?!#).*', '', line)                        for line in list2]    # <remove not uBO style trailing comments'/>
 
-print('1.4 : keep case only for cosmetic filter; apply lower case for the remaining')
+print(' 4/16 : keep case only for cosmetic filter; apply lower case for the remaining')
 # <keep case only for cosmetic filer; lower case for the remaining/>
 list2 = [line for line in list2 if re.search(r'#', line)] + [line.lower() for line in list2 if not(re.search(r'#', line))]
 
-print('1.5 : remove items leaded by ****::*')
+print(' 5/16 : remove items leaded by ****::*')
 list2 = [re.sub(r'....\:\:[0-9].*', '', line)                  for line in list2]    # <remove items leaded by '____::_'/>
 
-print('1.6 : remove IP addresses')
+print(' 6/16 : remove IP addresses')
 list2 = [re.sub(r'[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+.*', '', line) for line in list2]    # <remove IP addresses'/>
 
-print('1.7 : remove www.')
+print(' 7/16 : remove www.')
 list2 = [re.sub(r'www\.', '', line).strip()                    for line in list2]    # <remove www./>
 
-print('1.8 : remove trailing $all')
+print(' 8/16 : remove trailing $all')
 list2 = [re.sub(r'\$all$', '', line).strip()                   for line in list2]    # <remove trailing $all/>
 
-print('1.9 : remove trailing $third-party')
+print(' 9/16 : remove trailing $third-party')
 list2 = [re.sub(r'\$third-party$', '', line).strip()           for line in list2]    # <remove trailing $third-party/>
 
-print('1.10: remove trailing ^')
+print('10/16 : remove trailing ^')
 list2 = [re.sub(r'\^$', '', line).strip()                      for line in list2]    # <remove trailing ^/>
 
-print('1.11: remove ||')
+print('11/16 : remove ||')
 list2 = [re.sub(r'\|\|', '', line).strip()                     for line in list2]    # <remove ||/>
 
-print('1.12: remove leading 0.0.0.0 ')
+print('12/16 : remove leading 0.0.0.0 ')
 list2 = [re.sub(r'0\.0\.0\.0 ', '', line).strip()              for line in list2]    # <remove leading '0.0.0.0 '/>
 
-print('1.13: remove leading 127.0.0.1 ')
+print('13/16 : remove leading 127.0.0.1 ')
 list2 = [re.sub(r'127\.0\.0\.1 ', '', line).strip()            for line in list2]    # <remove leading '127.0.0.1 '/>
 
-print('1.14: remove leading ::1 ')
+print('14/16 : remove leading ::1 ')
 list2 = [re.sub(r'\:\:1 ', '', line).strip()                   for line in list2]    # <remove leading '::1 '/>
 
-print('1.15: remove items with len < 2')
-list2 = [line for line in list2 if len(line) > 1]                                     # <remove items with len < 2/>
+print('15/16 : remove items with length < 2')
+list2 = [line for line in list2 if len(line) > 1]                                     # <remove items with length < 2/>
 
-print('1.16: remove items leaded by localhost')
+print('16/16 : remove items leaded by localhost')
 list2 = [line for line in list2 if line[0:8] != 'localhost']                          # <remove items leaded by localhost />
 
 print(
@@ -140,7 +139,7 @@ list3 = [re.sub('r\$important$', '', line) for line in list3]
 
 print(
     '{:,}'.format(len(list3)),
-    'domains listed',
+    'listed',
     '\n'
     )
 
@@ -148,16 +147,16 @@ print(
 
 # <remove redundant domains from list>
 
-print('Dedup domains; this operation can take long, please wait')
+print('Dedup domains; this operation could take long time, please wait')
 print('--------------------------------------------------------')
 
-list2 = set(list2) - set(list3)
+list2 = set(list2) - set(list3)    # <only domains part are processed in this section/>
 
-i_max = math.ceil(math.log(len(list3) / 1e4) / math.log(2))
+i_max = round(math.log(len(list3) / 5e4) / math.log(2))
 for i in range(i_max, -1, -1) :
     n = round(len(list3) / (2**i))
     print(
-        'recursive size reduccion',
+        'recursive domain downsizing',
         '{:2.0f}'.format(i_max + 1 - i),
         '/',
         i_max,
@@ -166,7 +165,7 @@ for i in range(i_max, -1, -1) :
         'domains kept'
         )
     # <filter() + map() option>
-    list3 = list(map(lambda line: line if (len(list(filter(lambda substring: substring in line and len(line) > len(substring), list3[:n]))) == 0) else '', tqdm.tqdm(list3)))
+    list3 = list(map(lambda line: line if (len(list(filter(lambda substring: ('.' + substring) in line, list3[:n]))) == 0) else '', tqdm.tqdm(list3)))
     list3 = [line for line in list3 if len(line) > 0]
     # </filter() + map() option>
     
@@ -174,7 +173,7 @@ for i in range(i_max, -1, -1) :
     #list3 = [line for line in list3 if len(list(filter(lambda substring: substring in line and len(line) > len(substring), tqdm.tqdm()list3[:n])))) == 0]
     # </filter() + list comprehension option>
 
-list2 = sorted(set(list2) | set(list3))
+list2 = sorted(set(list2) | set(list3))    # <rebuild full list with shrinked domains part/>
 
 print(
     '\n',
