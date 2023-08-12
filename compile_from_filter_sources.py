3f36c9d52625c8ba10c9e5c84df78cfe94b667e0
mipiticuenta <mi.piticuenta@gmail.com>
2023-08-12 18:07:05 +0200

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -1,15 +1,12 @@
-''' Compile block list from url sources '''
+''' Compile a single deduplicated lock list from url sources '''
 
 # <product backlog>
-
-# improve treatment of domains$important filter
 
 # </product backlog>
 
 import os                   # Miscellaneous operating system interfaces
 import re                   # Regular expression operations
 import requests             # Get files using url
-import time                 # Time functions
 import tqdm                 # Progress bar
 import math                 # Math functions
 
@@ -25,9 +22,9 @@ print(
     '\n',
     '###################################################################################', '\n',
     '#', '\n',
-    '# Compile block list from url sources', '\n',
+    '# Compile a single deduplicated block list from url sources', '\n',
     '#', '\n',
-    '# input: <filter_sources> textfile', '\n',
+    '# input : <filter_sources> textfile', '\n',
     '# output: <compiled_block_list> textfile, sorted, deduplicated, without empty lines', '\n',
     '#', '\n',
     '###################################################################################', '\n',
@@ -36,10 +33,11 @@ print(
 
 # <get filter url sources from file, dedup and sort>
 
-list1 = [line for line in open(file1_in_name, encoding='UTF-8') if line != '']    # <populate from sources/>
-list1 = [line                      for line in list1 if line[0] != '!']           # <discard commented lines (leading !)/>
-list1 = [re.sub(r'\!.*', '', line) for line in list1]                             # <remove trailing comments'/>
-list1 = [line.strip()              for line in list1 if line != '']               # <remove leading/trailing spaces and discard empty lines/>
+list1 = [line.strip() for line in open(file1_in_name, encoding='UTF-8')]    # <populate lists from sources/>
+list1 = [line.strip()               for line in list1 if line != '']        # <remove leading/trailing spaces and discard empty lines/>
+list1 = [line                       for line in list1 if line[0] != '!']    # <discard commented lines (leading !)/>
+list1 = [re.sub(r'\ !.*', '', line) for line in list1]                      # <remove trailing comments'/>
+
 list1 = sorted(list1)
 
 # </get filter url sources from file, dedup and sort>
@@ -80,14 +78,15 @@ print('Removing unnecessary spaces, lines, comments, etc; applying lower case ex
 print('--------------------------------------------------------------------------------------------------------')
 
 print(' 1/16 : dedup spaces and remove leading/trailing spaces')
+list2 = [line for line in list2 if len(line) > 1]                                    # <remove items with length < 2/>
 list2 = [re.sub(r' +', ' ', line).strip()                      for line in list2]    # <dedup spaces and remove leading/trailing spaces/>
 
 print(' 2/16 : remove uBO style trailing comments')
-list2 = [re.sub(r'!.*', '', line).strip()                      for line in list2]    # <remove uBO style comments'/>
+list2 = [line for line in list2 if line[0] != '!']                                   # <remove uBO style comments'/>
 
 print(' 3/16 : remove not uBO style trailing comments')
-list2 = [re.sub(r'#.*', '', line).strip()                      for line in list2]    # <remove not uBO style trailing comments'/>
-list2 = [re.sub(r'^#(?!#).*', '', line)                        for line in list2]    # <remove not uBO style trailing comments'/>
+list2 = [line for line in list2 if line[0] != '#']                                   # <remove not uBO style trailing comments'/>
+list2 = [re.sub(r' #(?!#+).*', '', line)                       for line in list2]    # <remove not uBO style trailing comments'/>
 
 print(' 4/16 : keep case only for cosmetic filter; apply lower case for the remaining')
 # <keep case only for cosmetic filer; lower case for the remaining/>
@@ -124,10 +123,10 @@ print('14/16 : remove leading ::1 ')
 list2 = [re.sub(r'\:\:1 ', '', line).strip()                   for line in list2]    # <remove leading '::1 '/>
 
 print('15/16 : remove items with length < 2')
-list2 = [line for line in list2 if len(line) > 1]                                     # <remove items with length < 2/>
+list2 = [line for line in list2 if len(line) > 1]                                    # <remove items with length < 2/>
 
 print('16/16 : remove items leaded by localhost')
-list2 = [line for line in list2 if line[0:8] != 'localhost']                          # <remove items leaded by localhost />
+list2 = [line for line in list2 if line[0:8] != 'localhost']                         # <remove items leaded by localhost />
 
 print(
     '\n',
@@ -141,7 +140,7 @@ print(
 print('Listing domain filters: ', end = '')
 
 list3 = [line for line in list2 if re.search(r'^[a-z0-9[-_\.a-z0-9]+\.[a-z]+\.[a-z]+(\$important)?$', line) or re.search(r'^[a-z0-9][-_\.a-z0-9]+\.[a-z]+(\$important)?$', line)]
-list3 = [re.sub('r\$important$', '', line) for line in list3]    # <remove trailing $important; to be improved in next sprint/>
+list3 = [re.sub('r\$important$', '', line) for line in list3]    # <remove trailing $important from domains/>
 
 print(
     '{:,}'.format(len(list3)),
@@ -154,21 +153,34 @@ print(
 # <remove redundant domains from list>
 
 print('Dedup domains; this operation could take long time, please wait')
-print('--------------------------------------------------------')
+print('---------------------------------------------------------------')
 
-list2 = set(list2) - set(list3)    # <only domains part are processed in this section/>
-
-list3r = [line for line in list3 if re.search(r'^[a-z0-9][-_a-z0-9]+\.[a-z]+$', line)]    # <*.@ domains are elemental items/>
+list2  = set(list2) - set(list3)    # <only domains part are processed in this section/>
+list3r = [line for line in list3 if re.search(r'^[a-z0-9][-_a-z0-9]+\.[a-z]+$', line)]    # <@.@ domains are elemental items/>
 
 print(
     '{:,}'.format(len(list3r)),
-    'elemental *.@ domains found; removed from recursive doamin downsizing'
+    'elemental @.@ domains found; removed from recursive domain downsizing'
     )
 
-list3 = set(list3) - set(list3r)    # <elemental domains removed for faster size reduction, and added to final result/>
-list3 = sorted(list3, key = lambda x: -len(x))    # <sort by decreasing length for faster size reduction/>
+list3r3 = [line for line in list3 if re.search(r'^[a-z0-9][-_a-z0-9]+\.[a-z0-9][-_a-z0-9]+\.[a-z]+$', line)]    # <@.@.@ domains items/>
+list3  = set(list3) - set(list3r) - set(list3r3)    # <elemental domains removed for faster size reduction, and added to final result/>
+list3  = sorted(list3, key = lambda x: -len(x))    # <sort by decreasing length for faster size reduction/>
 
-i_max = round(math.log((len(list3) + len(list3r)) / 5e4) / math.log(2))
+print(
+    'recursive domain downsizing',
+    '{:2.0f}'.format(0),
+    '/',
+    '0',
+    ';',
+    '{:,}'.format(len(list3) + len(list3r3)),
+    'domains kept'
+    )
+list3r3 = list(map(lambda line: line if (len(list(filter(lambda substring: ('.' + substring) in line, list3r))) == 0) else '', tqdm.tqdm(list3r3)))
+list3r3 = [line for line in list3r3 if len(line) > 0]    # <cleanup empty lines/>
+list3r  = sorted(set(list3r) | set(list3r3))             # <compile deduplicated domains up to present stage/>
+
+i_max = round(math.log((len(list3) + len(list3r)) / 1e5) / math.log(2))
 for i in range(i_max, -1, -1) :
     list3_filter = list(set(list3) | set(list3r))
     n = round(len(list3_filter) / (2**i))
@@ -176,17 +188,17 @@ for i in range(i_max, -1, -1) :
         'recursive domain downsizing',
         '{:2.0f}'.format(i_max + 1 - i),
         '/',
-        i_max,
+        i_max + 1,
         ';',
         '{:,}'.format(len(list3)),
         'domains kept'
         )
     # <filter() + map() option>
     list3 = list(map(lambda line: line if (len(list(filter(lambda substring: ('.' + substring) in line, list3_filter[:n]))) == 0) else '', tqdm.tqdm(list3)))
-    list3 = [line for line in list3 if len(line) > 0]
+    list3 = [line for line in list3 if len(line) > 0]    # <cleanup empty lines/>
     # </filter() + map() option>
     
-    # <filter() + list comprehension option>
+    # <filter() + list comprehension option; may worth it a benchmark vs map()?>
     #list3 = [line for line in list3 if len(list(filter(lambda substring: ('.' + substring) in line, tqdm.tqdm(list3_filter[:n])))) == 0]
     # </filter() + list comprehension option>
 
@@ -209,8 +221,7 @@ filelist = os.listdir('.')
 
 if file2_out_name + '_old' in filelist :
     os.remove(file2_out_name + '_old')
-
-os.rename(file2_out_name, file2_out_name + '_old')
+    os.rename(file2_out_name, file2_out_name + '_old')
 
 print(
     'backup saved: '
