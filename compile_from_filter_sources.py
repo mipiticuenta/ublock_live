1a6247b0f08044dd41cc14ef1733f51717bd51dc
mipiticuenta <mi.piticuenta@gmail.com>
2023-08-21 01:20:40 +0200

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -151,7 +151,7 @@ list2s = [re.sub(r'.*domain=', '', line).strip() for line in list2s]            
 list2s = [re.sub(r'.*denyallow=', '', line).strip() for line in list2s]              # <remove leading .*denyallow=/>
 list2s = [re.sub(r'\,.*$', '', line).strip() for line in list2s]                     # <remove trailing .*,.*/>
 list2s = [line.split('|') for line in list2s]                                        # <flatten list'/>
-list2s = [item[0] for line in list2s for item in line]                               # <flatten list'/>
+list2s = [item[0] for line in list2s if len(line) > 0 for item in line]              # <flatten list'/>
 
 list2 = sorted(set(list2) | set(list2s))                                             # <join retrieved domains to main list'/>
 del(list2s)
@@ -223,11 +223,14 @@ list2 = [re.sub(r'[~,]$', '', line).strip() for line in list2]                  
 list2 = [re.sub(r'\.php\?$', '.', line) for line in list2]                           # <remove trailing .php?/>
 
 print('16/21 : remove /api .png /app /*/ lines ')
-list2 = [re.sub(r'^[/\.]js$', '', line) for line in list2]                           # <remove []/.]js lines />
+list2 = [re.sub(r'^\.js$', '', line) for line in list2]                              # <remove .js lines />
+list2 = [re.sub(r'^/js$', '', line) for line in list2]                               # <remove /js lines />
 list2 = [re.sub(r'^/api$', '', line) for line in list2]                              # <remove /api lines />
 list2 = [re.sub(r'^\.png$', '', line) for line in list2]                             # <remove .png lines />
+list2 = [re.sub(r'^\.jpg$', '', line) for line in list2]                             # <remove .jpg lines />
 list2 = [re.sub(r'^/app$', '', line) for line in list2]                              # <remove /app lines />
 list2 = [re.sub(r'^/?[a-z0-9]?/?$', '', line) for line in list2]                     # <remove /*/ lines />
+list2 = [re.sub(r'^[js/\*\.]+$', '', line) for line in list2]                        # <remove lines with combinations of js/*. />
 
 print('17/21 : remove /wp-content/uploads/.*')
 list2 = [re.sub(r'(?<=\w)/wp\-content/uploads/.*', '', line) for line in list2]      # <remove /wp-content/uploads/.*'/>
