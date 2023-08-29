309ce365975620fd6eb9ecfba31297718e7cc6a4
mipiticuenta <mi.piticuenta@gmail.com>
2023-08-29 22:00:53 +0200

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -202,6 +202,7 @@ while n_1 > len(list2):                                                         
 
     print('11/20 : clean up leading [-_.:~|*]+ * $ [-_./0-9]+ @/ asset asp cgi cfm gif htm http image jpg js mp4 php png static tiff www')
     list2 = [re.sub(r'^[-_\:\=\~\|\*\!0-9]+(?=[/\.\$$])', '', line) for line in list2]          # <remove leading [-_:=~|*!0-9]+ followed by /.$ />
+    list2 = [re.sub(r'^[a-z]\.', '', line).strip() for line in list2]                           # <remove leading a-z. />
     list2 = [re.sub(r'^\$', '*$', line).strip() for line in list2]                              # <fix leading $ with *$ />
     list2 = [re.sub(r'^[/\.\=\?]\$', '*$', line).strip() for line in list2]                     # <fix leading /$ .$ =$ ?$ with *$ />
     list2 = [re.sub(r'^\.?[-_a-z0-9\*]+/', '/', line) for line in list2]                        # <replace leading @/ with / />
