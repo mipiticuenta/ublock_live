3089082f318064de05969fd481972e316250885a
mipiticuenta <mi.piticuenta@gmail.com>
2023-09-02 23:05:15 +0200

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -561,7 +561,7 @@ print(
     '/',
     '3',
     ';',
-    '{:,}'.format(len(list3) + len(list3r3)),
+    '{:,}'.format(len(list3) + len(list3r)),
     'domains kept'
     )
 list3 = list(map(lambda line: line if (len(list(filter(lambda substring: ('.' + substring) in line, list3r))) == 0) else '', tqdm.tqdm(list3)))
