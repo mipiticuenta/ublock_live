7a82fe90905005b0de1c3dfce2574fdf3a85c60c
mipiticuenta <mi.piticuenta@gmail.com>
2023-11-05 16:27:48 +0100

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -50,7 +50,7 @@ dom_sw = input('Enter <y> to include domain deflation : ')
 # <test direct connection to internet>
 
 r = requests.get('https://github.com', timeout = 5)
-if r.status_code == 200:
+if r.status_code != 200:
     print('\nNo direct internet connection; trying alt proxy servers')
     proxy_servers  = proxy_servers_alt                                          # <no direct connection available, apply proxy_servers_alt/>
 
