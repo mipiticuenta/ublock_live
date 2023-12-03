cfd949a4f705b6b9519c1d30d47448747bbb46ba
mipiticuenta <mi.piticuenta@gmail.com>
2023-12-03 20:06:39 +0100

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -488,9 +488,16 @@ list9 = [line.strip() for line in open(file9_in_name, encoding='UTF-8')]        
 list9 = [re.sub(r'(^| +)!.*', '', line) for line in list9]                      # <remove comments' />
 list9 = [line for line in list9 if line.strip() != '']                          # <discard empty lines />
 
+print(list2)
+print(list5)
+
 for item in tqdm.tqdm(list9) :
     list2 = [re.sub(item, '', line) for line in list2]                          # <remove spurious filter from main list based on regex-white_list/>
     list5 = [re.sub(item, '', line) for line in list5]                          # <remove spurious filter from regex list based on regex-white_list />
+
+print(list2)
+print(list5)
+
 
 # </get regex white list from file, dedup, sort and clean up filters>
 
