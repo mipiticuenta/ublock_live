082c6294513ce3425cdb9d378941fc71d3e3c90b
mipiticuenta <mi.piticuenta@gmail.com>
2023-12-03 22:14:48 +0100

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -490,7 +490,7 @@ list9 = [line for line in list9 if line != '']                                  
 print(list2)
 print(list5)
 
-for pattern in tqdm.tqdm(list9) :
+for pattern in list9 :
     list2 = [re.sub(pattern, '', line) for line in list2]                       # <remove spurious filter from main list based on regex-white_list/>
     list5 = [re.sub(pattern, '', line) for line in list5]                       # <remove spurious filter from regex list based on regex-white_list />
 
