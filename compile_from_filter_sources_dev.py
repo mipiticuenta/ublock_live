438d53d2884b0b7f2da0b36437248b79f1af1a5a
mipiticuenta <mi.piticuenta@gmail.com>
2024-01-25 19:10:31 +0100

    update

--- compile_from_filter_sources_dev.py
+++ compile_from_filter_sources_dev.py
@@ -388,11 +388,12 @@ print(' 8/21 : split urls with $ domain=, denyallow= ')
 
 def f08(line):
 
-    if re.search(r'\$.*(domain|denyallow)=', line):
+    line = [line]
+
+#    if re.search(r'\$.*(domain|denyallow)=', line):
 #        line = re.sub(r'^.*(domain|denyallow)=', '', line).split('|') + [re.sub(r'\$.*$', '', line)]     # <split domains and  url part />
-        line = [re.sub(r'\$.*$', '', line)]
-    else:
-        [line]
+#    else:
+#        [line]
     return line
 
 pool = ThreadPool(thr)                                                          # <make the pool of workers />
