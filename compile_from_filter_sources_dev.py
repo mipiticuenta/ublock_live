721a5a95ce54c398c0606b1aaab553ab8189d374
mipiticuenta <mi.piticuenta@gmail.com>
2024-01-31 19:21:00 +0100

    update

--- compile_from_filter_sources_dev.py
+++ compile_from_filter_sources_dev.py
@@ -952,7 +952,7 @@ def f20_2(pattern):
         '{:3.0f}'.format((counter.value / counter_max) * 100), '% ',
         '(', '{:d}'.format(counter.value), '/', counter_max, ') ',
         '{:.0f}'.format((time() - t0) / 60), ' elapsed'
-        '{:.0f}'.format((time() - t0) / counter.value * (counter_max - counter.value) / 60), ' minutes remaining'',
+        '{:.0f}'.format((time() - t0) / counter.value * (counter_max - counter.value) / 60), ' minutes remaining',
         end = '\r',
         sep = '',
         flush = True
