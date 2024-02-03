3c62dc1e4c40319c67877daf2989f7a45f523719
mipiticuenta <mi.piticuenta@gmail.com>
2024-02-03 20:48:53 +0100

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -340,8 +340,8 @@ list2  = sorted(set(list2) - set(list5))
 list5 = [
     line
     for line in list5
-    if not re.search(r'^/.*[\^\?]\*.*/$', line)                                 # <remove wrong regex filter />
-    if not re.search(r'^/.*\/\$.*/$', line)                                     # <remove wrong regex filter />
+    if not re.search(r'^/.*[\^\?]\*.*/$', line)                                 # <remove wrong ^* ?* in regex filter />
+    if not re.search(r'^/.*/\$.*/$', line)                                      # <remove wrong /$ in regex filter />
     if not re.search(r'^/[\^\(]http', line)                                     # <remove [^(]http regex filter />
     if len(line) > 4
 ]
