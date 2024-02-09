bd19b0c41d3eb9eec5621c4fca6ff95ed7c9f801
mipiticuenta <mi.piticuenta@gmail.com>
2024-02-09 20:53:42 +0100

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -653,8 +653,8 @@ while n_1 > len(list2) :                                                        
         if re.search(r'^[-\.\w]+\^\*[-/\.\w]+', line) :
             line = re.sub(r'\^\*', '', line)                                    # <remove spurious ^*/>
 
-        if re.search(r'^[-\./\w]+$', line) :
-            line    = [re.sub(r'^.+(?=/[^/]+(?:/\*)?$)', '', line)]             # <simplify urls keeping last /* part />
+        if re.search(r'^\.?[^\.]*/[-\./\w]+$', line) :
+            line = [re.sub(r'^.+(?=/[^/]+(?:/\*)?$)', '', line)]                # <simplify urls keeping last /* part />
 
         return line
 
