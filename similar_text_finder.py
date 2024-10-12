cdeca76810ba8e4e0387b66ed8d98b82fc40243d
mipiticuenta <mi.piticuenta@gmail.com>
2024-10-12 19:30:38 +0200

    u

--- similar_text_finder.py
+++ similar_text_finder.py
@@ -22,7 +22,7 @@ import pandas as pd
 import re                                       # <regex capabilities />
 
 file1_in_name   = 'compiled_block_list'
-file2_out_name  = 'most_common_text_list'
+file2_out_name  = 'word_frequency'
 thr             = os.cpu_count()
 t_start         = time()
 
@@ -41,33 +41,43 @@ print(
     '# ============================================================\n',
 )
 
-# <get filter list >
+# <get words list >
 
-list1 = sorted(
-    list(
-        filter(
-            None,
-            [
-                re.sub(r'^.*[^\w\.\-].*$', '', line.strip())                    # <remove filters with any symbol except \.\-\_ />
-                for line in open(file1_in_name, encoding='UTF-8')
-            ]                                                                   # <populate source lists />
-        )                                                                       # <remove empty elements />
-    )
+list1 = list(
+    filter(
+        None,
+        [
+            re.sub(r'^[!\*].*$', '', line).strip()
+            for line in open(file1_in_name, encoding='UTF-8')
+        ]                                                                   # <populate filters />
+    )                                                                       # <remove empty elements />
 )
 
-# </get filter list >
+def f_split(line) :
+    line = re.sub(r'[_\W]+', ',', line)                                     # <split each word>
+    line = line.split(',')
+    return line
 
-# <populate list >
+pool = ThreadPool(thr)                                                      # <make the pool of workers />
+list1 = pool.map(f_split, list1)                                            # <execute function by multithreading />
+pool.close()                                                                # <close the pool and wait for the work to finish />
+pool.join()
 
-list1 = list(filter(None, sorted(set(list1))))                                  # <remove empty elements />
+list1 = [
+    line if (type(line) == str)                                             # <prevents word atomization into chars if word type />
+    else item
+    for line in list1
+    for item in line
+]                                                                           # <flatten list />
+
+list1 = [word for word in list1 if len(word) > 3]                     # <discard short words />
 
 print(
-    '\n',
     '{:,}'.format(len(list1)),
-    'unique filters gathered'
+    'words gathered\n'
 )
 
-# </populate list >
+# </get words list >
 
 # <process filter list>
 
@@ -78,21 +88,26 @@ print(
     sep = ''
 )
 
-def f_dist(series) :
-        return reduce(lambda x, y:dist(x, y), series)
-
 dist_df = pd.DataFrame()
-dist_df['filter'] = list1
-dist_df = dist_df.groupby('filter').agg(f_dist)
+dist_df['word'] = list1
 
-dist_df = dist_df.sort_values(by = ['sum_dist'], ascending = True)
+# <write main output>
 
 file2_out = dist_df.to_csv(file2_out_name, index = False)
-
-file2_out.close()
-
 print(
-    'Results saved to textfile <' + file2_out_name + '>\n'
+    'words saved to textfile <' + file2_out_name + '>\n'
+)
+
+# </write main output>
+
+dist_df['sum_dist'] = dist_df['word'].apply(lambda x: sum([distance(word, x) for word in dist_df['word']]))
+dist_df = dist_df.sort_values(by = ['sum_dist'], ascending = False)
+
+# <write main output>
+
+file2_out = dist_df.to_csv(file2_out_name, index = False)
+print(
+    'words saved to textfile <' + file2_out_name + '>\n'
 )
 
 # </write main output>
