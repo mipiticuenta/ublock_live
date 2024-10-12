34d185a62edb807b9e3bdcee642d12e692c466e2
mipiticuenta <mi.piticuenta@gmail.com>
2024-10-12 19:46:09 +0200

    update

--- find_word_freq.py
+++ find_word_freq.py
@@ -92,7 +92,7 @@ dist_df = pd.DataFrame()
 dist_df['word'] = list(filter(None, sorted(set(list1))))                    # <df deduplication />
 
 print(
-    '{:,}'.format(dist_df..shape[0]),
+    '{:,}'.format(dist_df.shape[0]),
     'unique words gathered\n'
 )
 
