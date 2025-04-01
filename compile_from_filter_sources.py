c7bb0c69a4990e722c79ea3f4d2dea308c1ac683
mipiticuenta <mi.piticuenta@gmail.com>
2025-04-01 22:13:14 +0200

    update

--- compile_from_filter_sources.py
+++ compile_from_filter_sources.py
@@ -769,11 +769,16 @@ while not converged :                                                           
         if re.search(r'^[-\w]+\\\.[-\w]+', line) :
             line = ''                                                           # <remove faulty \. />
 
+        if re.search(r'^[-\w]+\.\\[-\w]+', line) :
+            line = ''                                                           # <remove faulty .\ />
+
         if re.search(r'^[-\.\w]+\^\*[-/\.\w]+', line) :
             line = ''                                                           # <remove spurious ^*/>
 
         if re.search(r'^[\./]?[-\w]*/[-\./\w]+[-\.\w](?:/\*)?$', line) :
             line = re.sub(r'^.+(?=/[^/]+(?:/\*)?$)', '', line)                  # <simplify urls keeping last /* part />
+
+        line = re.sub(r'^/[-\.\w]*(?=[-\w]+\.[-\w]+$)', '', line)               # <simplify urls keeping last @.@ part />
 
         return line
 
